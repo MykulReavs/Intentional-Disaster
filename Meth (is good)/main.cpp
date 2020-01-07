9bcfcd74b9a4b5deaf53f7f87ecff305d969e994
MykulReavs <59621471+MykulReavs@users.noreply.github.com>
2020-01-07 20:41:02 +0100

    Add files via upload
    idk trying to get a role on a discord server

---
+++ Meth (is good)/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "Choose any number if I didn't messup \n";
+    cin >> a;
+
+    cout << "Choose another number" << endl;
+    cin >> b;
+
+    sum = a + b;
+    cout << "The total is if it works: \n" << sum << endl;
+
+    return 0;
+}
